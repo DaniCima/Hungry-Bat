8610e389553fbfb126b55caecbb758d82ce8ee6f
Daniela Trinchera <trinchera.dm@gmail.com>
2022-08-02 17:54:50 +0200

    second commit

--- game.js
+++ game.js
@@ -20,43 +20,65 @@ class Moth {
         mothImg.src = this.img;
         ctx.drawImage(mothImg,this.x, this.y, this.width, this.height);
     }
+
+    // createMoths () {
+    //   let minHeight = 10; // set params for the obstacles
+    //   let maxHeight = 550;
+    //   let minWidth = 20; // set params for the obstacles
+    //   let maxWidth = 200;
+    //   this.x = Math.floor(Math.random() * (maxHeight - minHeight + 1) + minHeight);
+    //   this.y = Math.floor(Math.random() * (maxWidth - minWidth + 1) + minWidth);
+    //   if (score==20) {
+    //     moths.push(new Moth(this.x, this.y, 20, 20, "./images/moth.png"));
+    //     moths.push(new Moth(this.x, this.y, 20, 20, "./images/moth2.png"));
+    //   } else if (score==40) {
+    //     moths.push(new Moth(this.x, this.y, 20, 20, "./images/moth.png"));
+    //     moths.push(new Moth(this.x, this.y, 20, 20, "./images/moth2.png"));
+    //     moths.push(new Moth(this.x, this.y, 20, 20, "./images/moth.png"));
+    //     moths.push(new Moth(this.x, this.y, 20, 20, "./images/moth2.png"));
+    //   } else {
+    //     moths.push(new Moth(this.x, this.y, 20, 20, "./images/moth.png"));
+    //     moths.push(new Moth(this.x, this.y, 20, 20, "./images/moth2.png"));
+    //     moths.push(new Moth(this.x, this.y, 20, 20, "./images/moth.png"));
+    //     moths.push(new Moth(this.x, this.y, 20, 20, "./images/moth2.png"));
+    //     moths.push(new Moth(this.x, this.y, 20, 20, "./images/moth.png"));
+    //     moths.push(new Moth(this.x, this.y, 20, 20, "./images/moth2.png"));
+    //   }
+    // }
+
+    moveMoths() {
+
+      // this.x= Math.floor(Math.random()*1100)+ 15;
+      // this.y= Math.floor(Math.random()*700)+ 15;
+     }
 }
 
 class Bat {
     constructor(){
       this.x = 50;
       this.y = 50;
-      this.speedX=0;
-      this.speedY=0;
+      // this.speedX=0;
+      // this.speedY=0;
       this.width = 50;
       this.height = 50;
-      this.imga = "./images/murci1.png"
+      this.flapping = true;
+      this.src= "./images/murci2mirror.png";
     }
     
     drawBat(){
       const batImg = new Image();
-      batImg.src = this.imga;
+      batImg.src = this.src;
       ctx.drawImage(batImg, this.x, this.y, this.width, this.height);
+      
     }
 
     movebat(keyCode){
       console.log('x', this.x);
       console.log('y', this.y);
       ctx.clearRect(this.x, this.y, this.width, this.height);
+      // this.x += this.speedX;
+      // this.y += this.speedY;
       
-    //   switch(keyCode){
-    //     case 37:
-    //     //Making sure car doesn't go off the road
-    //     if(this.x > 20){
-    //       this.x -= 10;
-    //     }
-    //       break;
-    //     case 39:
-    //     //Making sure car doesn't go off the road
-    //     if (this.x < 430 ){
-    //       this.x += 10;
-    //     }
-    //       break;
-    //   }
+      
     }
   }
--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+// let currentGame;
+
 let canvas = document.querySelector("canvas");
 const ctx = canvas.getContext('2d');
-let startButton = document.getElementById("start-button")
+let startButton = document.getElementById("start-button");
 let firstPage = document.querySelector("#first-page");
 let finishPage = document.querySelector("#game-over");
 let gameOver = false;
+let score = 0;
 let intervalId= 0;
+// let audio = new Audio("./images/0000971.mp3");
+// audio.volume = .1;
+
 
 let img = new Image();
 img.src ="../images/fondo2.jpg"
@@ -17,55 +23,79 @@ img.src ="../images/fondo2.jpg"
 // let batX = 50;
 // let batY = 50;
 
-
-let moth = new Image ();
-moth.src = "../images/moth2.png"
-let mothX = 250;
-let mothY = 100;
-
-
 window.onload = function () {
     canvas.style.display = "none";
     finishPage.style.display = 'none';
 
     startButton.addEventListener("click", () => {
         startGame();
+        checkGameOver();
     });
+    // currentGame = new Game();
 
+  
     // let batX += batSpeedX;
     // let batY += batSpeedY;
     const bat = new Bat();
-    const moth = new Moth(150, 250, 20, 20, "./images/moth.jpg");
-    const moth1 = new Moth( 500, 300, 20,20, "./images/moth2.png");
-    // const moth2 = new Moth(1000,600,20,20,)
-
-
+    
+    moth0 = new Moth(300, 250, 20, 20, "./images/moth.png");
+    moth1 = new Moth(500, 300, 20,20, "./images/moth2.png");
+    moth2 = new Moth(1000,600,20,20,"./images/moth2.png");
+    const moths = [moth0, moth1, moth2];
+    
+    let keysPressed= {};
     document.addEventListener("keydown", (event) => {
+        keysPressed[event.keyCode] = true;
         console.log(event.keyCode);
         switch(event.keyCode){
-            case 38: bat.y -=2;
+            case 38: bat.y -=4; //up
             break;
-            case 40: bat.y +=2;
+            case 40: bat.y +=4; //down
             break;
             case 37:
-              bat.x -= 2;
+              bat.x -= 4; // left
               break;
             case 39:
-              bat.x += 2;
+              bat.x += 4; // right
               break;
           }
-        ctx.clearRect(bat.speedX, bat.speedY, 50, 50);
+          if (keysPressed[38] && keysPressed[37]) {
+            bat.y -=3;
+            bat.x -=3;
+        }
+        if (keysPressed[38] && keysPressed[39]) {
+            bat.y -=3;
+            bat.x +=3;
+        }if (keysPressed[40] && keysPressed[37]) {
+            bat.y +=3;
+            bat.x -=3;
+        }if (keysPressed[40] && keysPressed[39]) {
+            bat.y +=3;
+            bat.x +=3;
+        }
+        ctx.clearRect(bat.x, bat.y, 50, 50);
 
         });
 
      document.addEventListener('keyup', (event) => {
-        bat.speedX= 0;
-        bat.speedY= 0;
+        delete keysPressed[event.keyCode];
+        // bat.speedX= 0;
+        // bat.speedY= 0;
      });
 
+     setInterval(() => {
+        bat.flapping = !bat.flapping
+    
+      }, 300);
 
+      function updateScore() {
+        ctx.font = '20px serif';
+        ctx.fillStyle = 'white';
+        ctx.fillText(`Score: ${score}`, 550, 50);
+      }
     
     function startGame() {
+        // intervalId += 1; // increment 
         firstPage.style.display = 'none';
         finishPage.style.display = 'none';
         canvas.style.display = 'block';
@@ -73,20 +103,53 @@ window.onload = function () {
 
     
         bat.drawBat();
-        moth.drawMoth();
-        moth1.drawMoth();
+        if (bat.flapping) {
+        bat.src = "./images/murci1.png";
+      } else {
+        bat.src = "./images/murci2mirror.png";
+      }
+       // audio.play();     
         
-        
+        moths.forEach(moth => moth.drawMoth());
+
+        // if(intervalId % 80 === 0){ 
+        // moth0.moveMoths()
+        // moth1.moveMoths()
+        // moth2.moveMoths()
+        // }
        // ctx.drawImage(bat,bat.x,bat.y,50,50);
       //  ctx.drawImage(moth,mothX,mothY,20,20);
+      detectCollision();
 
-
-
-
-        if (gameOver) {
+        if (gameOver === true) {
+            
             cancelAnimationFrame(intervalId);
+        
         } else {
-            intervalId= requestAnimationFrame(startGame)
+             intervalId= requestAnimationFrame(startGame)
         }
     }
+
+    function detectCollision() {
+        moths.forEach(moth => {
+            if (bat.x + bat.width >= moth.x && bat.x <= moth.x - moth.width && bat.y + bat.height <= moth.y && bat.y >= moth.y + moth.height) {
+                moth.x = -500;
+                score +=10;
+              }
+            // currentGame.moth.createMoths();
+        })
+        updateScore();
+    }
+    function checkGameOver() {
+        setTimeout(() => {
+            gameOver = true;
+        },3000);
+        // setTimeout(() => {
+        //     if (score<170) false
+        // },12000);
+        // setTimeout(() => {
+        //     if (score<300) false
+        // },18000);
+    }
+    f
 }
